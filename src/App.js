cd4555a5d5e2c5b0fcf310d8ad2bc00cf41cf0db
jeison guimaraes azevedo <jeison.azevedo.ads@gmail.com>
2018-12-06 08:41:27 -0200

    criação de botoes

--- src/App.js
+++ src/App.js
@@ -7,23 +7,47 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text,TouchableOpacity, View} from 'react-native';
+//import firebase from 'firebase';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
+
 
 type Props = {};
 export default class App extends Component<Props> {
+
+ /* componentWillMount () {
+    //Posso Fazer qualquer tipo de configuração global aqui como por exemplo o Firebase
+    if (firebase.apps.length === 0) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyBS-Ltt7gmDwVDtI5T3JzrNMlJ-ug66s_8",
+        authDomain: "flitter-db915.firebaseapp.com",
+        databaseURL: "https://flitter-db915.firebaseio.com",
+        projectId: "flitter-db915",
+        storageBucket: "flitter-db915.appspot.com",
+        messagingSenderId: "205532639397"
+      })
+    }
+  
+  }*/
+
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+       <TouchableOpacity style={styles.Button}>
+         <Text style={styles.Text}>     Inserir Item </Text>
+       </TouchableOpacity>
+       <TouchableOpacity style={styles.Button}>
+         <Text style={styles.Text}>     Lista Item </Text>
+       </TouchableOpacity>
+       <TouchableOpacity style={styles.Button}>
+         <Text style={styles.Text}>   Update Item </Text>
+       </TouchableOpacity>
+       <TouchableOpacity style={styles.Button}>
+         <Text style={styles.Text}>     Delete Item </Text>
+       </TouchableOpacity>
       </View>
     );
   }
@@ -32,6 +56,7 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection:'column',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -46,4 +71,21 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  Button:{
+    borderColor:'black',
+    padding:10,
+    justifyContent:'center',
+    width:200,
+    height:10,
+  },
+  Button:{
+    borderColor:'black',
+   borderWidth:1,
+   width:100,
+   textAlign:'center',
+   marginTop:30,
+   justifyContent:'center',
+   paddingTop:20,
+   paddingBottom:20,
+  }
 });
